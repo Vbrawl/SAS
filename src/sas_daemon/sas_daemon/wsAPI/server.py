e814d69700adefd42bd84b5bcb0099f8c0fa54cb
Vbrawl <greninjagr@gmail.com>
2024-05-07 22:00:13 +0300

    Added function to parse data as rules.

--- src/sas_daemon/sas_daemon/wsAPI/server.py
+++ src/sas_daemon/sas_daemon/wsAPI/server.py
@@ -1,5 +1,6 @@
 from typing import Callable, Coroutine, Any
 from sas_commons import Database, Template, PersonTemplateArguments, SendMessageRule
+from datetime import datetime, timedelta
 import json
 import asyncio
 import websockets
@@ -97,9 +98,9 @@ class WSAPI:
     def parse_as_person(self, kwargs:dict[str, Any], id_required:bool = False) -> PersonTemplateArguments:
         id = kwargs.get("id", None)
         if id_required and id is None:
-            raise RuntimeError("ID Required for object creation but is not present in the data")
+            raise TypeError("ID Required for object creation but is not present in the data")
         elif not isinstance(id, (int, type(None))):
-            raise RuntimeError("Invalid ID parameter")
+            raise TypeError("Invalid ID parameter")
 
         first_name:str|None = kwargs.get("first_name", None)
         if not isinstance(first_name, (str, type(None))): raise TypeError("Invalid FIRST_NAME parameter")
@@ -120,6 +121,52 @@ class WSAPI:
             telephone=telephone,
             address=address
         )
+    
+    def parse_as_rule(self, kwargs:dict[str, Any], id_required:bool = False) -> SendMessageRule:
+        id:int|None = kwargs.get("id", None)
+        if id_required and id is None:
+            raise TypeError("ID Required for object creation but is not present in the data")
+        elif not isinstance(id, (int, type(None))):
+            raise TypeError("Invalid ID parameter")
+        
+        recipientIds:list[int] = kwargs.get("recipients", [])
+        if not isinstance(recipientIds, list): raise TypeError("Invalid RECIPIENTS parameter")
+        recipients:list[PersonTemplateArguments] = list(map(lambda x: self.db.get_person(x), recipientIds)) # type: ignore
+        if any(map(lambda x: x is None, recipients)): raise TypeError("Invalid RECIPIENTS parameter values")
+
+        templateId:int = kwargs["template"]
+        if not isinstance(templateId, int): raise TypeError("Invalid TEMPLATE parameter")
+        template = self.db.get_template(templateId)
+        if template is None: raise TypeError("Invalid TEMPLATE parameter value")
+
+        start_date = datetime.strptime(kwargs["start_date"], "%Y-%m-%d %H:%M:%S.%f")
+        end_date_p = kwargs.get("end_date", None)
+        if not isinstance(end_date_p, (str, type(None))): raise TypeError("Invalid END_DATE parameter")
+        elif end_date_p is not None: end_date = datetime.strptime(end_date_p, "%Y-%m-%d %H:%M:%S.%f")
+        else: end_date = None
+
+        interval_days = kwargs.get("interval_days", 0)
+        interval_seconds = kwargs.get("interval_seconds", 0)
+        if not isinstance(interval_days, int): raise TypeError("Invalid INTERVAL_DAYS parameter")
+        if not isinstance(interval_seconds, int): raise TypeError("Invalid INTERVAL_SECONDS parameter")
+        interval = timedelta(days=interval_days, seconds=interval_seconds)
+
+        last_executed_p = kwargs.get("last_executed", None)
+        if not isinstance(last_executed_p, (str, type(None))): raise TypeError("Invalid LAST_EXECUTED parameter")
+        if last_executed_p is not None: last_executed = datetime.strptime(last_executed_p, "%Y-%m-%d %H:%M:%S.%f")
+        else: last_executed = None
+
+
+        return SendMessageRule(
+            recipients = recipients,
+            template = template,
+            start_date = start_date,
+            end_date = end_date,
+            interval = interval,
+            last_executed = last_executed,
+            id = id
+        )
+
     
     async def template_get(self, **kwargs) -> dict:
         try:
