10a2b071976ae3c70e9bfcabae95271128ebb5bb
Vbrawl <greninjagr@gmail.com>
2024-05-08 14:34:43 +0300

    Added rule collecting and modified send_sms to auto-update rule details.

--- src/sas_daemon/sas_daemon/daemon.py
+++ src/sas_daemon/sas_daemon/daemon.py
@@ -13,20 +13,22 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-from sas_commons import Database, SendMessageRule, PersonTemplateArguments
+from sas_commons import Database, SendMessageRule
 from wsAPI.server import WSAPI
 import asyncio
-import math
 
 
 
 class Daemon:
     def __init__(self):
+        self.collect_rules_interval = 60
+
         self.db = Database("temp.db")
         self.operations:dict[int, asyncio.Task] = {}
         self.op_lock = asyncio.Lock()
 
         self.wsapi = WSAPI(self.db)
+        # Update when a rule is updated, their attributes are automatically updated before sending.
         self.wsapi.OPTIONS["rule"]["add"] = self.rule_add_and_register # type: ignore
         self.wsapi.OPTIONS["rule"]["alter"] = self.rule_alter_and_register # type: ignore
         self.wsapi.OPTIONS["rule"]["remove"] = self.rule_deregister_and_remove # type: ignore
@@ -64,12 +66,35 @@ class Daemon:
                     del self.operations[id]
         return await wsapi.rule_remove(**kwargs)
 
-    async def send_sms(self, pta:PersonTemplateArguments, message:str): # callback
-        print(message)
+    async def send_sms(self, pta:SendMessageRule): # callback
+        template = pta.template
+        if pta.template.id is not None:
+            temp = self.db.get_template(pta.template.id)
+            if temp is not None:
+                template = temp
+
+        for recipient in pta.recipients:
+            if getattr(recipient, "id", None) is not None:
+                temp = self.db.get_person(recipient.id)
+                if temp is not None:
+                    recipient = temp
+            msg = template.compileFor(recipient)
+            # TODO: Use telnyx API to send a message
+            print(msg)
+    
+    async def collect_rules_indefinitely(self):
+        while True:
+            async with self.op_lock:
+                for k,v in self.operations.items():
+                    if v.done():
+                        del self.operations[k]
+            await asyncio.sleep(self.collect_rules_interval)
 
     async def start(self):
         for rule in self.db.get_rules():
             if rule.id is not None:
                 self.operations[rule.id] = asyncio.create_task(rule.infschedule(self.send_sms))
+        
+
         await self.wsapi.start_server()
-        await asyncio.sleep(math.inf)
+        await self.collect_rules_indefinitely()
