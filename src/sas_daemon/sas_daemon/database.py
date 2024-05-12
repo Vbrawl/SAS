98fd6ea9ce1470deaa4a6aed683b00fd298c7f57
Vbrawl <greninjagr@gmail.com>
2024-05-12 12:57:39 +0300

    Removed unecessary import

--- src/sas_daemon/sas_daemon/database.py
+++ src/sas_daemon/sas_daemon/database.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-from .templates import PersonTemplateArguments, TemplateArguments, Template
+from .templates import PersonTemplateArguments, Template
 from .rules import SendMessageRule
 from datetime import datetime, timedelta
 import sqlite3
