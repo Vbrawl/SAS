85e0cda593d6307116fb837e141cfc37c112f4b6
Vbrawl <greninjagr@gmail.com>
2024-05-18 12:50:03 +0300

    Extracted list code to a reusable class.

---
+++ src/sas_control_panel/src/js/DOMList.js
@@ -0,0 +1,176 @@
+
+
+
+
+(function(domlist) {
+    
+    domlist.DOMListItem = class {
+        constructor(obj) {
+            this.obj = obj;
+        }
+
+        add_section(item, class_name, content) {
+            const section = document.createElement("div");
+            section.classList.add(class_name);
+            section.textContent = content;
+
+            item.appendChild(section);
+        }
+
+        add_colon(item) {
+            const colon = document.createElement('span');
+            colon.classList.add('col');
+
+            item.appendChild(colon);
+        }
+
+        render(set_state__header, set_state__all, custom_handler = () => {}) {
+            const is_header = this.obj === null;
+
+            const item = document.createElement('div');
+            item.classList.add('list-item-wrapper');
+            if(is_header) {item.classList.add("list-header");}
+            this.customize_item(item);
+
+            this.add_colon(item);
+            const buttons = document.createElement('div');
+            buttons.classList.add("button-holder");
+
+            if(is_header) {
+                const button_label = document.createElement('div');
+                button_label.classList.add('button-holder-label');
+                button_label.textContent = "Select All";
+                buttons.appendChild(button_label);
+            }
+            const checkbox = document.createElement('input');
+            checkbox.classList.add('item-selector');
+            checkbox.setAttribute('type', 'checkbox');
+            if(is_header)
+                {checkbox.addEventListener('change', set_state__all);}
+            else
+                {checkbox.addEventListener('change', set_state__header);}
+            checkbox.addEventListener('change', custom_handler);
+
+            buttons.appendChild(checkbox);
+            item.appendChild(buttons);
+            return item;
+        }
+
+        customize_item(item) {
+            // NOTE: This is a virtual function and needs be implemented in subclasses.
+            throw 'Virtual function not implemented in subclass.';
+        }
+    }
+
+    domlist.DOMList = class {
+        constructor(dom_element) {
+            this.dom_element = dom_element;
+            this.items = [];
+        }
+
+        add_item(item) {
+            this.items.push(item);
+        }
+
+        add_items(items) {
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
+                this.add_item(item);
+            }
+        }
+
+        render(custom_handler = () => {}) {
+            this.dom_element.html = '';
+
+            for (let i = 0; i < this.items.length; i++) {
+                const item = this.items[i];
+                this.dom_element.appendChild(item.render(
+                    // NOTE: We use lambdas to keep the list as "this" inside the handlers.
+                    (evt) => {this.set_state__header(evt);},
+                    (evt) => {this.set_state__all(evt);},
+                    (evt) => {custom_handler(evt);}));
+            }
+        }
+
+        /**
+         * Set all checkboxes to match the header's one.
+         * @param {Event} evt The event object
+         */
+        set_state__all(evt) {
+            const list_items = this.dom_element.getElementsByClassName("item-selector");
+            const myState = evt.currentTarget.checked;
+
+            for (let i = 0; i < list_items.length; i++) {
+                const item_selector = list_items[i];
+                const item = item_selector.parentElement.parentElement;
+                if(item.classList.contains("list-header")) {continue;}
+
+                item_selector.checked = myState;
+            }
+        }
+
+        /**
+         * Set the header's checkbox state depending on if all
+         * checkboxes in the list are checked or not.
+         * @param {Event} evt The event object
+         */
+        set_state__header(evt) {
+            const item_selectors = this.dom_element.getElementsByClassName("item-selector");
+
+            var header_selector = null;
+            var all_checked = true;
+
+            for (let i = 0; i < item_selectors.length; i++) {
+                const item = item_selectors[i];
+
+                if(item.parentElement.parentElement.classList.contains("list-header")) {
+                    header_selector = item;
+                } else if (!item.checked) {all_checked = false;}
+            }
+
+            if(header_selector) header_selector.checked = all_checked;
+        }
+    }
+
+    domlist.TemplateDOMListItem = class extends domlist.DOMListItem {
+        customize_item(item) {
+            const is_header = this.obj === null;
+            if(!is_header) {item.setAttribute("data-id", this.obj.id);}
+
+            this.add_section(item, "template-label", is_header ? "Label" : this.obj.label);
+            this.add_colon(item);
+            this.add_section(item, "template-message", is_header ? "Message" : this.obj.message);
+        }
+    }
+
+    domlist.PeopleDOMListItem = class extends domlist.DOMListItem {
+        customize_item(item) {
+            const is_header = this.obj === null;
+            if(!is_header) {item.setAttribute("data-id", this.obj.args.id);}
+
+            this.add_section(item, "first-name", is_header ? "First Name" : this.obj.args.first_name);
+            this.add_colon(item);
+            this.add_section(item, "last-name", is_header ? "Last Name" : this.obj.args.last_name);
+            this.add_colon(item);
+            this.add_section(item, "telephone", is_header ? "Telephone" : this.obj.args.telephone);
+            this.add_colon(item);
+            this.add_section(item, "address", is_header ? "address" : this.obj.args.address);
+        }
+    }
+
+    domlist.RuleDOMListItem = class extends domlist.DOMListItem {
+        customize_item(item) {
+            const is_header = this.obj === null;
+            if(!is_header) {item.setAttribute("data-id", this.obj.id);}
+
+            this.add_section(item, "rule-label", is_header ? "Label" : this.obj.label);
+            this.add_colon(item);
+            this.add_section(item, "recipient-count", is_header ? "Recipient Count" : obj.recipients.length);
+            this.add_colon(item);
+            this.add_section(item, "start-date", is_header ? "Start Date" : sasapi.date_to_string(this.obj.start_date));
+            this.add_colon(item);
+            this.add_section(item, "end-date", is_header ? "End Date" : (this,obj.end_date ? sasapi.date_to_string(this.obj.end_date) : ''));
+        }
+    }
+
+}(window.domlist = window.domlist || {}));
--- src/sas_control_panel/src/js/view.js
+++ src/sas_control_panel/src/js/view.js
@@ -3,134 +3,6 @@ let page_object_type = GETparams.get("object_type");
 if(page_object_type == null) {page_object_type = "template";}
 
 
-function add_section(item, class_name, textContent) {
-    const section = document.createElement("div");
-    section.classList.add(class_name);
-    section.textContent = textContent;
-
-    item.appendChild(section);
-}
-
-function add_colon(item) {
-    const sep = document.createElement('span');
-    sep.classList.add("col");
-
-    item.appendChild(sep);
-}
-
-function dom_list_item(obj_name, obj, is_header = false) {
-    const item = document.createElement("div");
-    item.classList.add("list-item-wrapper");
-    if(is_header) {item.classList.add("list-header");}
-    else {item.setAttribute("data-id", obj_name == "people" ? obj.args.id : obj.id);}
-
-    switch (obj_name) {
-        case "template":
-            add_section(item, "template-label", is_header ? "Label" : obj.label);
-            add_colon(item);
-            add_section(item, "template-message", is_header ? "Message" : obj.message);
-            break;
-        case "people":
-            add_section(item, "first-name", is_header ? "First Name" : obj.args.first_name);
-            add_colon(item);
-            add_section(item, "last-name", is_header ? "Last Name" : obj.args.last_name);
-            add_colon(item);
-            add_section(item, "telephone", is_header ? "Telephone" : obj.args.telephone);
-            add_colon(item);
-            add_section(item, "address", is_header ? "Address" : obj.args.address);
-            break;
-        case "rule":
-            console.log(obj);
-            add_section(item, "rule-label", is_header ? "Label" : obj.label);
-            add_colon(item);
-            add_section(item, "recipient-count", is_header ? 'Recipient Count' : obj.recipients.length);
-            add_colon(item);
-            add_section(item, "start-date", is_header ? "Start Date" : sasapi.date_to_string(obj.start_date));
-            add_colon(item);
-            add_section(item, "end-date", is_header ? "End Date" : (obj.end_date ? sasapi.date_to_string(obj.end_date) : ''));
-            break;
-    }
-
-
-    add_colon(item);
-    const buttons = document.createElement('div');
-    buttons.classList.add("button-holder");
-
-
-    if(is_header) {
-        const button_label = document.createElement('div');
-        button_label.classList.add("button-holder-label");
-        button_label.textContent = "Select All";
-        buttons.appendChild(button_label);
-    }
-    const checkbox = document.createElement('input');
-    checkbox.classList.add("item-selector");
-    checkbox.setAttribute("type", "checkbox");
-    if(is_header) checkbox.addEventListener("change", set_all_state);
-    else checkbox.addEventListener("change", set_header_state)
-    checkbox.addEventListener("change", set_action_buttons_state);
-    buttons.appendChild(checkbox);
-
-    item.appendChild(buttons);
-    return item;
-}
-
-async function fill_list(object_name, objects, dom_element) {
-    dom_element.html = '';
-
-    dom_element.appendChild(
-        dom_list_item(object_name, null, true)
-    );
-
-    for (let i = 0; i < objects.length; i++) {
-        const obj = objects[i];
-        const item = dom_list_item(object_name, obj);
-        dom_element.appendChild(item);
-    }
-}
-
-
-/**
- * Set all checkboxes to match the header's one.
- * @param {Event} evt The event object
- */
-function set_all_state(evt) {
-    const list = evt.currentTarget.parentElement.parentElement.parentElement;
-    const myState = evt.currentTarget.checked;
-    const list_items = list.getElementsByClassName("item-selector");
-
-    for (let i = 0; i < list_items.length; i++) {
-        const item_selector = list_items[i];
-        const item = item_selector.parentElement.parentElement;
-        if(item.classList.contains("list-header")) {continue;}
-
-        item_selector.checked = myState;
-    }
-}
-
-/**
- * Set the header's checkbox state depending on if all
- * checkboxes in the list are checked or not.
- * @param {Event} evt The event object
- */
-function set_header_state(evt) {
-    const list = evt.currentTarget.parentElement.parentElement.parentElement;
-    const item_selectors = list.getElementsByClassName("item-selector");
-
-    var header_selector = null;
-    var all_checked = true;
-
-    for (let i = 0; i < item_selectors.length; i++) {
-        const item = item_selectors[i];
-
-        if(item.parentElement.parentElement.classList.contains("list-header")) {
-            header_selector = item;
-        } else if (!item.checked) {all_checked = false;}
-    }
-
-    if(header_selector) header_selector.checked = all_checked;
-}
-
 
 function set_action_buttons_state(evt) {
     const list = evt.currentTarget.parentElement.parentElement.parentElement;
@@ -193,7 +65,7 @@ async function delete_action(object_name) {
  */
 var client = new sasapi.Client();
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const dlist = new domlist.DOMList(document.getElementsByClassName("list")[0]);
     const delete_button = document.getElementsByClassName("delete-button")[0];
     const edit_button = document.getElementsByClassName("edit-button")[0];
 
@@ -215,29 +87,41 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    client.connect("127.0.0.1", 8585);
-
-    client.ws.onopen = async (evt) => {
+    client.connect("127.0.0.1", 8585, async (evt) => {
+        let temp_items = [];
         var items = [];
 
         switch (page_object_type) {
             case "template":
-                items = await client.template_get();
+                temp_items = await client.template_get();
+                items.push(new domlist.TemplateDOMListItem(null));
+                for (let i = 0; i < temp_items.length; i++) {
+                    const item_obj = temp_items[i];
+                    items.push(new domlist.TemplateDOMListItem(item_obj));
+                }
                 break;
 
             case "people":
-                items = await client.people_get();
+                temp_items = await client.people_get();
+                items.push(new domlist.PeopleDOMListItem(null));
+                for (let i = 0; i < temp_items.length; i++) {
+                    const item_obj = temp_items[i];
+                    items.push(new domlist.PeopleDOMListItem(item_obj));
+                }
                 break;
 
             case "rule":
-                items = await client.rule_get();
+                temp_items = await client.rule_get();
+                items.push(new domlist.RuleDOMListItem(null));
+                for (let i = 0; i < temp_items.length; i++) {
+                    const item_obj = temp_items[i];
+                    items.push(new domlist.RuleDOMListItem(item_obj));
+                }
                 break;
         }
+        delete temp_items;
 
-        fill_list(
-            page_object_type,
-            items,
-            document.getElementsByClassName("list")[0]
-        );
-    }
+        dlist.add_items(items);
+        dlist.render(set_action_buttons_state);
+    });
 });
