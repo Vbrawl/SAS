516931dc9b553678b2f580cd371e3cae74985ed7
Vbrawl <greninjagr@gmail.com>
2024-05-19 13:19:59 +0300

    Removed sasd.service in favor to the install.sh script

---
+++ install/install.sh
@@ -0,0 +1,34 @@
+
+DAEMON_DIR="/opt/SAS"
+PYTHON_EXE="/bin/python"
+SERVICE_DIR="/etc/systemd/system"
+
+DAEMON_SOURCE="../src/sas_daemon"
+
+# Create daemon directory
+mkdir -p $DAEMON_DIR
+
+# Create virtual environment directory
+$PYTHON_EXE -m venv "${DAEMON_DIR}/.venv"
+
+# Load virtual environment and install sas_daemon inside of it
+source "${DAEMON_DIR}/.venv/bin/activate"
+pip install $DAEMON_SOURCE
+
+# Create the service file
+cat > "${SERVICE_DIR}/sasd.service" << EOF
+[Unit]
+Description=Send Automated SMS
+After=network.target
+
+[Service]
+WorkingDirectory=$DAEMON_DIR
+ExecStart=${DAEMON_DIR}/.venv/bin/sas-daemon
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload daemons and enable service
+systemctl daemon-reload
+systemctl enable --now sasd.service
